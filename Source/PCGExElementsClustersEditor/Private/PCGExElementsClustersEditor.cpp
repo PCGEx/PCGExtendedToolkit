3f9b33c1b9475bf9e3eee6fd0ef1a7d8f54ef996
Tim <nebukam@gmail.com>
2026-02-26 18:33:53 -0500

    [QoL] Added adjacency data viz

---
+++ Source/PCGExElementsClustersEditor/Private/PCGExElementsClustersEditor.cpp
@@ -0,0 +1,28 @@
+﻿// Copyright 2026 Timothé Lapetite and contributors
+// Released under the MIT license https://opensource.org/license/MIT/
+
+#include "PCGExElementsClustersEditor.h"
+
+#include "PCGExAssetTypesMacros.h"
+#include "PropertyEditorDelegates.h"
+#include "PropertyEditorModule.h"
+#include "Details/PCGExAdjacencySettingsCustomization.h"
+
+#define LOCTEXT_NAMESPACE "FPCGExElementsClustersEditorModule"
+
+#undef LOCTEXT_NAMESPACE
+
+PCGEX_IMPLEMENT_MODULE(FPCGExElementsClustersEditorModule, PCGExElementsClustersEditor)
+
+void FPCGExElementsClustersEditorModule::StartupModule()
+{
+	IPCGExEditorModuleInterface::StartupModule();
+
+	PCGEX_REGISTER_CUSTO_START
+	PCGEX_REGISTER_CUSTO("PCGExAdjacencySettings", FPCGExAdjacencySettingsCustomization)
+}
+
+void FPCGExElementsClustersEditorModule::ShutdownModule()
+{
+	IPCGExEditorModuleInterface::ShutdownModule();
+}
