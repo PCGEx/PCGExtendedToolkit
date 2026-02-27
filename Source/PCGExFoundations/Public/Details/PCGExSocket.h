65e83afb58d2d6361faf1dd8a4c29e60e213e032
Tim <nebukam@gmail.com>
2026-02-27 10:41:44 -0500

    [Packaging] Desperate attempt at fixing arm64 packaging

--- Source/PCGExFoundations/Public/Details/PCGExSocket.h
+++ Source/PCGExFoundations/Public/Details/PCGExSocket.h
@@ -12,12 +12,21 @@ struct PCGEXFOUNDATIONS_API FPCGExSocket
 {
 	GENERATED_BODY()
 
-	FPCGExSocket();
-	FPCGExSocket(const FName& InSocketName, const FVector& InRelativeLocation, const FRotator& InRelativeRotation, const FVector& InRelativeScale, FString InTag);
-	FPCGExSocket(const FName& InSocketName, const FTransform& InRelativeTransform, const FString& InTag);
-	~FPCGExSocket();
-	FPCGExSocket(const FPCGExSocket&);
-	FPCGExSocket& operator=(const FPCGExSocket&);
+	FPCGExSocket() = default;
+
+	FPCGExSocket(const FName& InSocketName, const FVector& InRelativeLocation, const FRotator& InRelativeRotation, const FVector& InRelativeScale, FString InTag)
+		: SocketName(InSocketName),
+		  RelativeTransform(FTransform(InRelativeRotation.Quaternion(), InRelativeLocation, InRelativeScale)),
+		  Tag(InTag)
+	{
+	}
+
+	FPCGExSocket(const FName& InSocketName, const FTransform& InRelativeTransform, const FString& InTag)
+		: SocketName(InSocketName),
+		  RelativeTransform(InRelativeTransform),
+		  Tag(InTag)
+	{
+	}
 
 	/** Whether this socket is managed by the system. */
 	UPROPERTY(meta=(PCG_NotOverridable, EditCondition="false", EditConditionHides))
